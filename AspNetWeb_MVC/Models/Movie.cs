18ec3ca4018de2d440321f7f62c07721e45a670f
ChuanmingXie <ChuanmingXie@outlook.com>
2022-02-27 00:16:49 +0800

    AspNetWeb_Mvc-为模型属性数据添加验证

--- AspNetWeb_MVC/Models/Movie.cs
+++ AspNetWeb_MVC/Models/Movie.cs
@@ -7,7 +7,7 @@ namespace AspNetWeb_MVC.Models
     public class Movie
     {
         public int Id { get; set; }
-
+        [StringLength(60,MinimumLength =3)]
         [Display(Name ="电影名称")]
         public string Title { get; set; }
 
@@ -16,12 +16,19 @@ namespace AspNetWeb_MVC.Models
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
         [Display(Name ="流派")]
         public string Genre { get; set; }
 
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Display(Name ="价格")]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z][a-zA-Z'\s]*$")]
+        [StringLength(5)]
         [Display(Name ="限制级别")]
         public string Rating { get; set; }
     }
