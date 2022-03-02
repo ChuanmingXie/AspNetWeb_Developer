fc7d1da0767065071ee4eff447d7d1c46752765a
ChuanmingXie <ChuanmingXie@outlook.com>
2022-03-02 21:33:46 +0800

    AspNetMvc_EFdbFirst-在ASP.NET MVC5中通过EF Database First修改并更新数据库结构

--- AspNetMvc_EFdbFirst_DB/Student.sql
+++ AspNetMvc_EFdbFirst_DB/Student.sql
@@ -3,6 +3,7 @@
 [StudentID] INT IDENTITY(1,1) NOT NULL,
 [LastName]  NVARCHAR(50) NULL,
 [FirstName] NVARCHAR(50) NULL,
+[MiddleName] Nvarchar(50) NULL,
 [EnrollmentDate] DateTime NULL,
 PRIMARY KEY CLUSTERED([StudentID] ASC)
 )
