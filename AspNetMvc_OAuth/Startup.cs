9550b980ef19287937e96fd9742964aa2c23e93d
ChuanmingXie <ChuanmingXie@outlook.com>
2022-03-03 09:32:01 +0800

    AspNetMvc_OAuth-该应用程序使用户能够使用 OAuth 2.0 和来自外部身份验证提供程序（如 Facebook、Twitter、LinkedIn、Microsoft 或 Google）的凭据登录。

---
+++ AspNetMvc_OAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvc_OAuth.Startup))]
+namespace AspNetMvc_OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
