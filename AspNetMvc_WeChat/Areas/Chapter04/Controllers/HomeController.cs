b01979558c9e09f4bf93de16afaf467e5af5926b
ChuanmingXie <ChuanmingXie@outlook.com>
2022-03-06 08:57:22 +0800

    AspNetMvc_WeChat-微信公众平台自定义菜单-Chapter04

---
+++ AspNetMvc_WeChat/Areas/Chapter04/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetMvc_WeChat_Base.WeChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_WeChat.Areas.Chapter04.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Chapter04/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MenuCreate()
+        {
+            ViewBag.menuCreate = WeChatMenuService.Create(Server.MapPath("~/Script/wechat-menu.json"));
+            return View();
+        }
+    }
+}
