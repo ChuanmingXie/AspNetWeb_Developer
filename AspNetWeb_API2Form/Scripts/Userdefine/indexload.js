9ad2f52209e190d49477e634a22699ad0b94b1a5
ChuanmingXie <ChuanmingXie@outlook.com>
2022-03-14 18:41:05 +0800

    AspNetWeb_API2Form-Microsoft课程-在Web Form中使用WebAPI-前端Js

---
+++ AspNetWeb_API2Form/Scripts/Userdefine/indexload.js
@@ -0,0 +1,12 @@
+﻿
+var uri="api/products"
+
+$(function () {
+    $.getJSON(uri, function (data) {
+        $('#products').empty();
+        $.each(data, function (key, item) {
+            var row = '<td>' + item.Name + '</td><td>' + item.Price + '</td>';
+            $('<tr/>', { html: row }).appendTo($('#products'));
+        });
+    });
+})
